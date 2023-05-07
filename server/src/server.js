ce528c64f8aa8f1e04c5e98bb5b423b03d0026ca
Mark Louis <marklouisbernardo318@gmail.com>
2023-05-07 12:35:31 +0800

    Second Comiit

--- server/src/server.js
+++ server/src/server.js
@@ -23,6 +23,8 @@ app.use(logger);
 dbConnect();
 //routes
 
-app.listen(5000, ()=>{
-    console.log('Server is listening on port 5000');
+const port = process.env.PORT || 5000;
+
+app.listen(port, ()=>{
+    console.log(`Server is listening on ${port}`);
 })
